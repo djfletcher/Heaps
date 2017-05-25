63727895c87a56a73539aa2de847bdaa974317ed
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-25 00:58:26 -0700

    Write max_heapify method

--- lib/heap.rb
+++ lib/heap.rb
@@ -4,6 +4,8 @@ class MaxHeap
     Heap.new(arr)
   end
 
+  attr_reader :heap
+
   def initialize(arr)
     @heap = arr
   end
@@ -22,16 +24,37 @@ class MaxHeap
 
   def max_heapify(idx)
     # no need to max_heapify if idx is a leaf
-    return self[idx] if idx > self.size / 2
-    left_idx = 2 * (idx + 1)
-    right_idx = left_idx + 1
+    return self[idx] if is_leaf?(idx)
+    left_idx, right_idx = child_indices(idx)
 
-    # if the max heap is violated at idx
-    if self[idx] < self[left_idx] || self[idx] < self[right_idx]
-      larger = self[left_idx] > self[right_idx] ? left_idx : right_idx
+    # no need to heapify unless the max heap is violated at idx
+    if out_of_place?(idx)
+      larger = larger(left_idx, right_idx)
       self[idx], self[larger] = self[larger], self[idx]
       max_heapify(larger)
     end
+  end
+
+  def is_leaf?(idx)
+    idx > self.size / 2
+  end
+
+  def out_of_place?(idx)
+    left_idx, right_idx = child_indices(idx)
+    self[idx] < self[left_idx] ||
+    right_idx && self[idx] < self[right_idx]
+  end
+
+  def larger(left_idx, right_idx)
+    return left_idx if !right_idx
+    self[left_idx] > self[right_idx] ? left_idx : right_idx
+  end
+
+  def child_indices(idx)
+    left = 2 * idx + 1
+    # there is no right child if left child is last element in heap
+    right = (left == self.size - 1) ? nil : left + 1
+    [left, right]
   end
 end
 
@@ -52,3 +75,10 @@ end
 def insert(heap, el)
 
 end
+
+
+
+
+test_heap = MaxHeap.new([6, 9, 8, 4, 7, 3, 2])
+test_heap.max_heapify(0)
+puts test_heap.heap == [9, 7, 8, 4, 6, 3, 2]
